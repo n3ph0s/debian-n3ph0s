61e9f7114d8ed3dc76c710038208712ac8faa277
n3ph0s <7236078+n3ph0s@users.noreply.github.com>
2023-10-01 16:47:00 +0800

    Updated Script

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # debian-n3ph0s
 Debian 12 Installation 
+
+## To Install:
+
+```
+sudo apt install git -y
+git clone https://github.com/n3ph0s/debian-n3ph0s.git
+cd debian-n3ph0s
+chmod +x install.sh
+sudo ./install.sh
+```
--- install.sh
+++ install.sh
@@ -11,11 +11,8 @@ username=$(id -u -n 1000)
 # Update package list and system
 apt update && apt upgrade -y
 
-# Install Core Programs
-apt install feh kitty rofi picom thunar lxpolkit x11-xserver-utils unzip curl pulseaudio-utils pipewire pavucontrol build-essential libx11-dev libxinerama-dev apt-transport-https -y
-
-# Install Quality of Life programs
-apt install neofetch flameshot psmisc neovim lxappearance papirus-icon-theme fonts-noto-color-emoji polybar dunst conky lightdm i3 -y
+# Install Core and Quality of Life Programs
+apt install feh kitty rofi picom thunar lxpolkit x11-xserver-utils unzip curl pulseaudio-utils pipewire pavucontrol build-essential libx11-dev libxinerama-dev apt-transport-https neofetch flameshot psmisc neovim lxappearance papirus-icon-theme fonts-noto-color-emoji polybar dunst conky zsh-syntax-highlighting ripgrep blueman bluez-tools bat exa tldr htop ranger zsh lightdm slick-greeter i3 -y
 
 # Install Brave Browser
 
@@ -25,24 +22,13 @@ echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=
 apt update && apt install brave-browser -y
 
 # Clone Wallpapers
-git clone https://gitlab.com/dwt1/wallpapers.git /home/$username/
-
-# Install Starship Prompt
-curl -sS https://starship.rs/install.sh | sh
-
-# Install ZSH
-apt install zsh -y
-chsh -s /usr/bin/zsh $username
-
-# Enable graphical login and change target from CLI to GUI
-systemctl enable lightdm
-systemctl set-default graphical.target
+git clone https://gitlab.com/dwt1/wallpapers.git /home/$username/wallpapers
 
 # Dotfile configuration
-git clone https://github.com/n3ph0s/dotfiles.git /home/$username/
-cd /home/$username/dotfiles
-bash install.sh
+git clone https://github.com/n3ph0s/dotfiles.git /home/$username/dotfiles
 cd /home/$username
+chown -R $username:$username dotfiles/
+chmow +x /home/$username/dotfiles/install.sh
 
 # Cleanup 
 rm -rf /home/$username/debian-n3ph0s
