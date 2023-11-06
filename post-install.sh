d4eb133477ec7c66e1bcc76dd578602fd3c1b2c5
n3ph0s <7236078+n3ph0s@users.noreply.github.com>
2023-11-06 09:40:04 +0800

    removed the use of neovim appimage and reverted back to stable version

--- post-install.sh
+++ post-install.sh
@@ -20,19 +20,6 @@ flatpak install --noninteractive --assumeyes com.discordapp.Discord
 cd /home/$username/dotfiles
 ./install.sh
 
-# Install and Configure NeoVim
-source /home/$username/.config/zsh/.zshrc
-mkdir /home/$username/appimage && cd /home/$username/appimage
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage 
-chmod u+x nvim.appimage
-sudo ln -s /home/$username/appimage/nvim.appimage /usr/local/bin/nvim.appimage
-set -u
-sudo update-alternatives --install /usr/bin/ex ex "${CUSTOM_NVIM_PATH}" 110
-sudo update-alternatives --install /usr/bin/vi vi "${CUSTOM_NVIM_PATH}" 110
-sudo update-alternatives --install /usr/bin/view view "${CUSTOM_NVIM_PATH}" 110
-sudo update-alternatives --install /usr/bin/vim vim "${CUSTOM_NVIM_PATH}" 110
-sudo update-alternatives --install /usr/bin/vimdiff vimdiff "${CUSTOM_NVIM_PATH}" 110
-
 # Cleanup
 cd /home/$username
 rm -rf debian-n3ph0s
